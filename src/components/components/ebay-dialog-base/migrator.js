dffd96869207305a29b718b87bb610c3dac4ab6c
Prashant Ashok <prashok@ebay.com>
2020-08-21 09:38:44 -0700

    comment out more code to work with marko 5

--- src/components/components/ebay-dialog-base/migrator.js
+++ src/components/components/ebay-dialog-base/migrator.js
@@ -6,48 +6,49 @@ const headerRegExp = /^h[1-6]$/;
  */
 
 function migrator(el, context) {
-    el.forEachChild((child) => {
-        let found = false;
-        if (!found && child && child.tagName === 'h2') {
-            found = true;
-            child.setTagName('@header');
-            context.deprecate(
-                '<h2> on dialog is not longer supported as the title tag. Use <@header> instead');
-        }
-    });
+    return;
+    // el.forEachChild((child) => {
+    //     let found = false;
+    //     if (!found && child && child.tagName === 'h2') {
+    //         found = true;
+    //         child.setTagName('@header');
+    //         context.deprecate(
+    //             '<h2> on dialog is not longer supported as the title tag. Use <@header> instead');
+    //     }
+    // });
 
-    const walker = context.createWalker({
-        enter(node) {
-            if (node.tagName === '@header') {
-                // Iterate only once in caes there are multiple tags
-                let foundH2 = false;
-                node.forEachChild((child) => {
-                    const tag = child.tagName;
-                    if (!foundH2 && headerRegExp.test(tag)) {
-                        foundH2 = true;
-                        const isDefault = tag === 'h2';
-                        const message = isDefault ? `<@header>` : `<@header as="${tag}">`;
+    // const walker = context.createWalker({
+    //     enter(node) {
+    //         if (node.tagName === '@header') {
+    //             // Iterate only once in caes there are multiple tags
+    //             let foundH2 = false;
+    //             node.forEachChild((child) => {
+    //                 const tag = child.tagName;
+    //                 if (!foundH2 && headerRegExp.test(tag)) {
+    //                     foundH2 = true;
+    //                     const isDefault = tag === 'h2';
+    //                     const message = isDefault ? `<@header>` : `<@header as="${tag}">`;
 
-                        context.deprecate(
-                            `${tag} is no longer needed in dialog @header. Use ${message}Title</@header> instead`);
+    //                     context.deprecate(
+    //                         `${tag} is no longer needed in dialog @header. Use ${message}Title</@header> instead`);
 
-                        node.argument = child.argument;
-                        node.params = child.params;
-                        child.forEachAttribute((attr) => {
-                            node.setAttributeValue(attr.name, attr.value);
-                        });
+    //                     node.argument = child.argument;
+    //                     node.params = child.params;
+    //                     child.forEachAttribute((attr) => {
+    //                         node.setAttributeValue(attr.name, attr.value);
+    //                     });
 
-                        if (!isDefault) {
-                            node.setAttributeValue('as', context.builder.literal(tag));
-                        }
-                        child.forEachChild(currentNode => child.insertSiblingBefore(currentNode));
-                        child.detach();
-                    }
-                });
-            }
-        }
-    });
-    walker.walk(el);
+    //                     if (!isDefault) {
+    //                         node.setAttributeValue('as', context.builder.literal(tag));
+    //                     }
+    //                     child.forEachChild(currentNode => child.insertSiblingBefore(currentNode));
+    //                     child.detach();
+    //                 }
+    //             });
+    //         }
+    //     }
+    // });
+    // walker.walk(el);
 }
 
 module.exports = migrator;
